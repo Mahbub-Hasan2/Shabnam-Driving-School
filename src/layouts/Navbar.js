3123cc96fb1d55659265d82d37c90dc8d5d08504
Mahbub-Hasan2 <mahbubdev1@gmail.com>
2023-04-10 13:24:03 +0600

    adde cursor pointer in nav links

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -27,16 +27,16 @@ const Navbar = () => {
           <div className={`items-center justify-between ${navOpen ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`} id="navbar-sticky">
             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <LinkScroll to="HOME" spy={true} smooth={true} className="block py-2 pl-3 pr-4 text-secondary rounded hover:bg-primary md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</LinkScroll>
+                <LinkScroll to="HOME" spy={true} smooth={true} className="cursor-pointer block py-2 pl-3 pr-4 text-secondary rounded hover:bg-primary md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</LinkScroll>
               </li>
               <li>
-                <LinkScroll to="ABOUT" spy={true} smooth={true} className="block py-2 pl-3 pr-4 text-secondary rounded hover:bg-primary md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</LinkScroll>
+                <LinkScroll to="ABOUT" spy={true} smooth={true} className="cursor-pointer block py-2 pl-3 pr-4 text-secondary rounded hover:bg-primary md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</LinkScroll>
               </li>
               <li>
-                <LinkScroll to="SERVICES" spy={true} smooth={true} className="block py-2 pl-3 pr-4 text-secondary rounded hover:bg-primary md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</LinkScroll>
+                <LinkScroll to="SERVICES" spy={true} smooth={true} className="cursor-pointer block py-2 pl-3 pr-4 text-secondary rounded hover:bg-primary md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</LinkScroll>
               </li>
               <li>
-                <LinkScroll to="CONTACT" spy={true} smooth={true} className="block py-2 pl-3 pr-4 text-secondary rounded hover:bg-primary md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</LinkScroll>
+                <LinkScroll to="CONTACT" spy={true} smooth={true} className="cursor-pointer block py-2 pl-3 pr-4 text-secondary rounded hover:bg-primary md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</LinkScroll>
               </li>
             </ul>
           </div>
